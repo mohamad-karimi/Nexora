bfd1fb77a7a74d2be8402b6ff4b03a4f09b46b92
mohamad-karimi <karimi.mohamad0011@gmail.com>
2026-09-02 11:57:51 +0330

    Add other pages to the website app
    Add 404, about and contact page to the website app and set url and view for them.

--- core/website/urls.py
+++ core/website/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name="home"),
+    path('404/', views.Page404View.as_view(), name="404"),
+    path('about/', views.AboutView.as_view(), name="about"),
+    path('contact/', views.ContactView.as_view(), name="contact"),
 ]
--- core/website/views.py
+++ core/website/views.py
@@ -3,3 +3,12 @@ from django.views.generic import TemplateView
 # Create your views here.
 class IndexView(TemplateView):
     template_name = 'website/index.html'
+
+class Page404View(TemplateView):
+    template_name = 'website/page-404.html'
+
+class AboutView(TemplateView):
+    template_name = 'website/page-about.html'
+
+class ContactView(TemplateView):
+    template_name = 'website/page-contact.html'
